56fbf3e144c700f1301dd4738c7075968fbd5e06
Yunus Emre Onen <ynsonen@gmail.com>
2022-08-21 14:12:44 +0300

    first commit

---
+++ Kodlama.ioBootcamp/MyDictionaryExample/MyDictionary.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionaryExample
+{
+    public class MyDictionary<K,V>
+    {
+        // Class Properties, not gettable and not settable from outside Class.
+        private K[] key { get; set; }
+        private V[] value { get; set; }
+
+        //Default Constructor Method  
+        public MyDictionary()
+        {
+            key= new K[0];
+            value= new V[0];
+        }
+       
+        //Add Method for MyDictionary Class
+        public void Add(K item1,V item2)
+        {
+            //I Created the temporary arrays.
+            K[] temporary = key;
+            V[] temporary2 = value;
+
+            //I increased the length of the property arrays.
+            key = new K[key.Length + 1];
+            value = new V[key.Length + 1];
+
+            //I have put the elements from the temporary array into the extended array.
+            for (int i = 0; i < temporary.Length; i++)
+            {
+                key[i] = temporary[i];
+                value[i] = temporary2[i];
+            }
+            // I have put the new parameters in extended arrays.
+            key[key.Length - 1] = item1;
+            value[key.Length - 1] = item2;
+        }
+
+        //Writing Method for key and value objects in MyDictionary Class
+        public void Write()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine($"{key[i].ToString()} ----------> {value[i].ToString()}");
+            }
+        }
+
+    }
+}
---
+++ Kodlama.ioBootcamp/MyDictionaryExample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MyDictionaryExample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // I have created object from MyDictionary Class.
+            MyDictionary<char,bool> dictionary = new MyDictionary<char,bool>();
+            
+            // I used to Method Add
+            dictionary.Add('A', true);
+            dictionary.Add('B', false);
+            dictionary.Add('C', true);
+            dictionary.Add('D', true);
+            
+            //I used to Write Method in the MyDictionary Class
+            dictionary.Write();
+            Console.WriteLine(dictionary);
+
+        }
+    }
+}
