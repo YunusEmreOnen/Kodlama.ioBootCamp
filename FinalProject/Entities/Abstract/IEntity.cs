87c6a85d4e2dd78beaabb41a46a23099aafaa87f
Yunus Emre Onen <ynsonen@gmail.com>
2022-08-28 17:23:36 +0300

    study7

---
+++ FinalProject/Entities/Abstract/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    //Every class that implements the IEntity Interface is a database object.
+    public interface IEntity
+    {
+
+    }
+}
