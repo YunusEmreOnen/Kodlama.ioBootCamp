acf15e4d07b2c0e9716d50bfc0e0ef4cb46f1b8c
Yunus Emre Onen <ynsonen@gmail.com>
2022-09-02 00:13:28 +0300

    study8

--- FinalProject/DataAccess/Abstract/IEntityRepository.cs
+++ FinalProject/DataAccess/Abstract/IEntityRepository.cs
@@ -1,18 +1,23 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //Generic Constraint
+    //class : Referans tip olabilir.
+    //IEntity : IEntity olabilir yada IEntity Arayüzünü implemente eden bir nesne olabilir.
+    //new() :  referansı oluşturulmuş olmalı.
+    public interface IEntityRepository<T> where T : class, IEntity, new()
     {
-        List<T> GetAll(Expression<Func<T,bool>> filter=null);
-        T Get();
+        List<T> GetAll(Expression<Func<T, bool>>? filter = null);
+        T Get(Expression<Func<T, bool>>? filter);
         void Add(T entity);
         void Update(T entity);
         void Delete(T entity);
-        List<T> GetAllByCategory(int categoryId);
     }
 }
--- FinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
+++ FinalProject/DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -3,6 +3,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,11 +17,11 @@ namespace DataAccess.Concrete.InMemory
         {
             _products = new List<Product>()
             {
-                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStoct=15},
-                new Product{ProductId=2,CategoryId=1,ProductName="Tabak",UnitPrice=500,UnitsInStoct=3},
-                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=150,UnitsInStoct=2},
-                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStoct=65},
-                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStoct=1}
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Tabak",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=150,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1}
             };
         }
         public void Add(Product product)
@@ -45,9 +46,19 @@ namespace DataAccess.Concrete.InMemory
             _products.Remove(productToDelete);
         }
 
+        public Product Get(Expression<Func<Product, bool>>? filter)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Product> GetAll()
         {
             return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Product> GetAllByCategory(int categoryId)
@@ -63,7 +74,7 @@ namespace DataAccess.Concrete.InMemory
             productToUpdate.ProductName = product.ProductName;
             productToUpdate.CategoryId = product.CategoryId;
             productToUpdate.UnitPrice = product.UnitPrice;
-            productToUpdate.UnitsInStoct= product.UnitsInStoct;
+            productToUpdate.UnitsInStock= product.UnitsInStock;
             
         }
     }
