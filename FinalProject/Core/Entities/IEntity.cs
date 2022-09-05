d65a102ab9efe202d3db58e7cfc71c7378f4aaf4
Yunus Emre Onen <ynsonen@gmail.com>
2022-09-05 07:47:54 +0300

    study9

--- FinalProject/Core/Entities/IEntity.cs
+++ FinalProject/Core/Entities/IEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //Every class that implements the IEntity Interface is a database object.
     public interface IEntity
---
+++ FinalProject/DataAccess/Abstract/IOrderDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using System;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IOrderDal:IEntityRepository<Order>
+    {
+    }
+}
