c1fefe97294cd26d9de17c47ef8e4d1a83f547b7
Yunus Emre Onen <ynsonen@gmail.com>
2022-09-06 15:56:36 +0300

    study10

---
+++ FinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //Yapıcı metotlarda metot overloadı sırasında kendi kendimizi tekrar etmemek için
+        //this anahtar sözcüğü ile belirlenen parametre için sınıfın farklı bir yapıcı metotdu
+        //çağrılabilir.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;           
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success
+        {
+            get;
+        }
+
+        public string Message
+        { 
+            get;
+        }
+    }
+}
