c7e0a4b5f0413bc013ad0844b3db555c7820aa49
Yunus Emre Onen <ynsonen@gmail.com>
2022-09-27 23:13:11 +0300

    Study14refactor

--- FinalProject/Core/Utilities/Security/Hashing/HashingHelper.cs
+++ FinalProject/Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -12,18 +12,18 @@ namespace Core.Utilities.Security.Hashing
         public static void CreatePasswordHash
             (string password,out byte[] passwordHash,out byte[] passwordSalt)  
         {
-            using (var hmc = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
-                passwordSalt = hmc.Key;
-                passwordHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(password));
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
             }
         }
 
         public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using (var hmc = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
-                var computedHash = hmc.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
                     if (computedHash[i] != passwordHash[i])
